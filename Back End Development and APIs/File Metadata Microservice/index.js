f21ca0ad443d64db843a4a97cadda3369b6eb768
Daniel Tsiu <35878710+Tinyblitz@users.noreply.github.com>
2024-12-19 22:22:57 -0500

    Create File Metadata Microservice Folder

--- Back End Development and APIs/File Metadata Microservice/index.js
+++ Back End Development and APIs/File Metadata Microservice/index.js
@@ -1,24 +1,22 @@
 const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
-const upload = multer({ dest: './public/uploads/' })
+const multer = require('multer');    // Required to deal with file uploads
 require('dotenv').config()
 
-var app = express();
-
-app.use(cors());
+const app = express();
 app.use('/public', express.static(process.cwd() + '/public'));
+
+// Send uploaded files to /public/uploads
+const upload = multer({ dest: './public/uploads/' })   // Files are made to be interactable since we only want the metadata
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// Search for uploaded file @ /api/fineanalyse and extract metadata
 app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
   const { originalname, mimetype, size } = req.file;
   return res.json({name: originalname, type: mimetype, size});
 })
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
